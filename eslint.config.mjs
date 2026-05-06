0b6383d43f69bea70a513b10b6d187b4ef638542
Võ Hoàng Phúc <contact@vohoangphuc.com>
2026-05-07 00:16:30 +0700

    chore: configure React ESLint settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,16 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    settings: {
+      react: {
+        version: "19.2.5",
+      },
+    },
+    rules: {
+      "react/display-name": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
